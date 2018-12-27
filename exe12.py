b6aa4a4dc7a80aec39d0976e747193b3822f9cec
Marco Sousa <mak.lp@hotmail.com>
2018-12-27 11:20:05 -0200

    primeiro commit

--- exe12.py
+++ exe12.py
@@ -1,4 +1,4 @@
 import math
 
-print (round(math.pi,1))
+print (round(math.pi,2))
 #arrendonda as casa decimais,  round(numero que quero arredondar, quantidade de casas a mostrar)
---
+++ exe13.py
@@ -0,0 +1,22 @@
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+
+place_up = str.upper(place)
+# Print out place and place_up
+print(place)
+print(place_up)
+
+
+# Print out the number of o's in place
+print(place.count('o'))
